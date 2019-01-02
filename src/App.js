6c0d7edd69d55e0de8e5a038ccb749cd32d8bc58
Soomin Kimu <soominkimu@gmail.com>
2019-01-02 23:36:02 +0900

    Canvas disappears

--- src/App.js
+++ src/App.js
@@ -1,34 +1,165 @@
 // weather-view
 import React from 'react';
 import './App.css';
+import {
+  IsLeapYear
+} from './CalUtil';
+import {
+  tempPercent,
+  //humidityPercent,
+  heatMapColor
+} from './WeatherUtil';
+
+const drawLine = (ctx, x, y, x2, y2) => {
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+}
+
+// drawing grid with rect():   O(n^2)
+//              with lineTo(): O(2n)
+const drawGrid = (ctx, x, y, gw, gh, W, H) => {
+  const x2 = x + W;
+  const y2 = y + H;
+  for (let xv=x; xv <= x2; xv += gw) { // vertical lines
+    drawLine(ctx, xv, y, xv, y2);
+  }
+  for (let yv=y; yv <= y2; yv += gh) {  // horizontal lines
+    drawLine(ctx, x, yv, x2, yv);
+  }
+}
+
+const gridVerticalYear = (ctx, x, y, gh, W) => {
+  const H = gh*366;
+  ctx.beginPath();
+  ctx.strokeRect(x, y, W, H);
+  const x2   = x + W;
+  const days = [31, 29, 31, 30, 31, 30,
+                31, 31, 30, 31, 30, 31];
+  let y2=y;
+  days.forEach(n => {
+    y2 += n*gh;
+    drawLine(ctx, x, y2, x2, y2);
+  });
+}
 
 class CanvasCompo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.w = 4;
+    this.h = 2;
+    //this.updateCanvas = this.updateCanvas.bind(this);
+  }
+
   componentDidMount() {
+    import('./dataJSON/M_tokyo-Avg.json')  // dynamic import
+      .then(module => {
+        this.weather = module;
+        this.updateCanvas();
+      })
+      .catch(err => console.log(err));
     this.updateCanvas();
   }
+
+  componentDidUpdate() {
+    this.updateCanvas();
+  }
+
   updateCanvas() {
     const ctx = this.refs.canvas.getContext('2d');
-    const w = 2;
-    for (let y=0; y<200; y++) {
-      for (let d=0; d<365; d++) {
-        ctx.fillStyle = `hsla(${Math.floor(240*Math.random())},100%,50%,1)`;
-        ctx.fillRect(10+w*d,10+w*y, w,w);
+    const m = {x: 10, y: 30};
+    const w = this.w;
+    const h = this.h;
+ //   const yrFr = Number(this.weather.meta.from.substring(0,4));
+    let bLeap = IsLeapYear(2018);
+    let y=0;  // year count
+    let d=0;  // day (data) count, 0 at the first day of each year
+    let p = {x: m.x, y: m.y};  // to save calculations
+ //   console.log(yrFr, this.weather.data.length, 'lines of data');
+
+    const lh = 20;  // legend height
+    let grd = ctx.createLinearGradient(m.x, m.y, w*(143+1), 0);
+    const pct_max = 1.2;
+    const pct_min = -.2;
+    const pct_w   = pct_max - pct_min;
+    const dpct    = pct_w/30;
+    for (let pct=pct_min; pct <= pct_max; pct += dpct)
+      grd.addColorStop((pct-pct_min)/pct_w, heatMapColor(pct, 1));
+    // ctx.filter = 'blur(4px)';
+    //ctx.filter = 'drop-shadow(4px 4px 2px lightgray)';
+    ctx.save();  // because we are using very expensive process (shadow)
+    ctx.shadowColor = 'LightGray';
+    ctx.shadowOffsetX = 4;
+    ctx.shadowOffsetY = 4;
+    ctx.shadowBlur = 4;
+    ctx.fillStyle = grd;
+    ctx.fillRect(m.x, m.y-lh-6, w*143, lh);
+    ctx.restore();
+    /*
+    let gd = ctx.createLinearGradient(m.x, m.y, m.x, m.y+h*366);
+    weather.data.forEach(data => {
+      if (data !== null) {
+        gd.addColorStop(d/366, heatMapColor(tempPercent(data), 1));  // hsla
+      }
+      if (d >= 365) {
+        ctx.fillStyle = gd;
+        ctx.fillRect(p.x, m.y, w, h*366);
+        y++;
+        p.x += w;
+        bLeap = IsLeapYear(yrFr + y);
+        d = 0;
+        p.y = m.y;  // carriage return
+        gd = ctx.createLinearGradient(m.x, m.y, m.x, m.y+h*366);
+      } else {
+        d++;
+        p.y += h;
+        if (!bLeap && (d === (31+28))) {
+          ctx.fillStyle = "Gray";      // Delete this line to fill with the same color as Feb.28's data
+          ctx.fillRect(p.x, p.y, w, h);
+          d++;  // skip to make up 1 day of Feb.29 for the non-leap year
+          p.y += h;
+        }
       }
     }
-    let grd = ctx.createLinearGradient(10,0,w*365,0);
-    grd.addColorStop(0,   "blue");
-    grd.addColorStop(.25, "cyan");
-    grd.addColorStop(.5,  "green");
-    grd.addColorStop(.75, "yellow");
-    grd.addColorStop(1,   "red");
-    // ctx.filter = 'blur(4px)';
-    ctx.filter = 'drop-shadow(4px 4px 2px lightgray)';
-    ctx.fillStyle = grd;
-    ctx.fillRect(10,10+w*201,w*365,40);
+    ctx.fillStyle = gd;
+    ctx.fillRect(p.x, m.y, w, h*366);
+    */
+/*
+    this.weather.data.forEach(data => {
+      if (data !== null) {
+        ctx.fillStyle = heatMapColor(tempPercent(data), 1);  // hsla
+        ctx.fillRect(p.x, p.y, w, h);
+      }
+      if (d >= 365) {
+        y++;
+        p.x += w;
+        bLeap = IsLeapYear(yrFr + y);
+        d = 0;
+        p.y = m.y;  // carriage return
+      } else {
+        d++;
+        p.y += h;
+        if (!bLeap && (d === (31+28))) {
+          ctx.fillStyle = "Gray";      // Delete this line to fill with the same color as Feb.28's data
+          ctx.fillRect(p.x, p.y, w, h);
+          d++;  // skip to make up 1 day of Feb.29 for the non-leap year
+          p.y += h;
+        }
+      }
+    });
+*/
+    ctx.lineWidth = .5;
+    ctx.strokeStyle = "rgba(0,0,0, .5)";
+    drawGrid(ctx, m.x, m.y, w, h, w*(y+1), h*366);
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "rgba(0,0,0, 1)";
+    gridVerticalYear(ctx, m.x, m.y, h, w*(y+1));
   }
+
   render() {
     return (
-      <canvas ref="canvas" width={1200} height={2000} />
+      <canvas ref="canvas" width={40+4*144} height={40+2*366*3} />
     );
   }
 }
--- src/WeatherUtil.js
+++ src/WeatherUtil.js
@@ -1,9 +1,9 @@
 // WeatherUtil.js
-const scaleRange = {  // for heatmap display
+const scaleRange = {
   temp : {
     min : 0,
-    max : 32,
-    w   : 32-0
+    max : 30,
+    w   : 30-0
   },
   humidity : {
     min : 10,
@@ -14,13 +14,13 @@ const scaleRange = {  // for heatmap display
 
 // CCS hsl color code from [0,1] val; based on the 5 color heatmap
 const itemPercent = (v, item) => {
-  if (v < item.min) return 0;
-  if (v > item.max) return 1;
+  //if (v < item.min) return 0;
+  //if (v > item.max) return 1;
   return Number(((v-item.min)/item.w).toFixed(2));
 }
 
-export const tempPercent     = (val) => itemPercent(val, scaleRange.temp);
-export const humidityPercent = (val) => itemPercent(val, scaleRange.humidity);
+export const tempPercent     = (temp) => itemPercent(temp, scaleRange.temp);
+export const humidityPercent = (hum)  => itemPercent(hum,  scaleRange.humidity);
 
 export const perc2Temp = (perc) => Number(((scaleRange.temp.w)*perc).toFixed(1));
 
@@ -28,14 +28,16 @@ export const perc2Temp = (perc) => Number(((scaleRange.temp.w)*perc).toFixed(1))
 //0    : blue   (hsl(240, 100%, 50%))
 //0.25 : cyan   (hsl(180, 100%, 50%))
 //0.5  : green  (hsl(120, 100%, 50%))
-//0.75 : yellow (hsl(60,  100%, 50%))
-//1    : red    (hsl(0,   100%, 50%))
+//0.75 : yellow (hsl( 60, 100%, 50%))
+//1    : red    (hsl(  0, 100%, 50%))
 // https://stackoverflow.com/questions/12875486/what-is-the-algorithm-to-create-colors-for-a-heatmap
 // hue: red primary at 0°, green at 120°, blue at 240°
-export const heatMapColor = (val, op=.7) => {
-  let v = val;
-  if      (v < 0) v = 0;
-  else if (v > 1) v = 1;
-  const h = Number(((1.0 - v) * 240).toFixed(2)); // converting to Number erases trailing 0's
-  return `hsla(${h},100%,50%,${op})`;
+export const heatMapColor = (p, op=.7) => {
+  const lfactor = 200;
+  let l=50;
+  if      (p > 1) l -= (p-1)*lfactor;
+  else if (p < 0) l -= -p*lfactor;
+  //  if      (p < 0) p = 0;
+  // else if (p > 1) p = 1;
+  return `hsla(${Number(((1.0 - p)*240).toFixed(2))},100%,${l}%,${op})`;
 }
